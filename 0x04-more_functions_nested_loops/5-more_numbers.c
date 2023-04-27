f93b94d6e817361250c8127d2748794e7d710e48
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-04-27 13:14:41 -0700

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,16 @@
 void more_numbers(void)
 {
 	int num = 0;
-
-	while (num <= 14)
+	int i = 10;
+	while (i--)
 	{
-		if (num >= 10)
-			_putchar(1 + '0');
-		_putchar(num % 10 + '0');
-		num++;
+		while (num <= 14)
+		{
+			if (num >= 10)
+				_putchar(1 + '0');
+			_putchar(num % 10 + '0');
+			num++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
