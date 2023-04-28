143c1c02f73ecf7a6dac74653b547dbbc9a6e321
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-04-27 18:36:08 -0700

    task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,11 +12,7 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n < 10 && n >= 0)
-	{
-		_putchar((n % 10) + '0');
-		return;
-	}
-	print_number(n / 10);
+	if (n / 10)
+		print_number(n / 10);
 	_putchar((n % 10) + '0');
 }
