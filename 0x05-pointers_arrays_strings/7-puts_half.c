67ba6e68494254d84607c67fe7d0304be87d2940
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-02 07:22:04 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,11 @@ void puts_half(char *str)
 {
 	int i;
 	int len = 0;
+	int start_point;
 
 	for (i = 0; str[i]; i++)
 		len++;
-	int start_point = (len + 1) / 2;
-
+	start_point = (len + 1) / 2;
 	for (i = start_point; str[i]; i++)
 		_putchar(str[i]);
 	_putchar('\n');
