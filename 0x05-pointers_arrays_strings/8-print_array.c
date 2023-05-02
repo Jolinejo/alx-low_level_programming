153a41ac9c495331820f26f79a96578d87361a01
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-02 09:48:24 -0700

    task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,8 @@ void print_array(int *a, int n)
 		if (i != n - 1)
 			printf("%d, ", a[i]);
 		else
-			printf("%d'\n'", a[i]);
+			printf("%d\n", a[i]);
 	}
 	if (n < 0)
-		_putchar('\n');
+		printf("\n");
 }
