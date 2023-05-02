feaa6d04ddc9ad42d3a6f27bdf51c3373ccd67e3
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-02 06:41:02 -0700

    task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * Description: to check if letter is uppercase
+ * @str: string to print
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
