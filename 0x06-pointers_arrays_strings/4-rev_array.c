4418f3bd6ac497ee6943bf62b3717536d032556a
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-04 11:51:14 -0700

    task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * Description: to check if letter is uppercase
+ * @a: array
+ * @n: length
+ * Return: int
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (int i = 0; i < (n - 1) / 2; i++)
+	{
+		int temp = *(a + n - 1 - i);
+		*(a + n - 1 - i) = *(a + i);
+		*(a + i) = temp;
+	}
+}
