b68c142445f4f6d19029ebcf6f4405b25220797c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-04 11:53:08 -0700

    task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (int i = 0; i < (n - 1) / 2; i++)
+	for (i = 0; i < (n - 1) / 2; i++)
 	{
 		int temp = *(a + n - 1 - i);
 		*(a + n - 1 - i) = *(a + i);
