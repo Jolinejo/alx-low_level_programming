8748ae141a04088a7f969dae59f93fba7f8a2f19
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-04 07:44:14 -0700

    task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * Description: to concat two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: dest modified
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; src[j] && j < n; j++)
+		dest[j] = src[j];
+	dest[j] = 0;
+	return (dest);
+}
