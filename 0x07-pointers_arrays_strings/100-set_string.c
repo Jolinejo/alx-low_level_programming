f9227404f92b41ab88cd1c841ae7554e85047623
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-08 17:28:01 -0700

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: source striing
+ * @to: dest string
+ * Description: to check if letter is uppercase
+ * Return: no return
+ */
+char *set_string(char **s, char *to)
+{
+	*s = to;
+	return (*s);
+}
