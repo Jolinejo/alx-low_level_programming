39955f103828024f8bf6dd7be9a1d1bbb5d84340
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-12 13:08:51 -0700

    task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * checker - Entry point
+ * Description: to check if letter is uppercase
+ * @n: number
+ * @i: divisor
+ * Return: int
+ */
+int checker(int n, int i)
+{
+	if (i > n / 2)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (checker(n, i + 1));
+}
+/**
+ * is_prime_number - Entry point
+ * Description: to check if letter is uppercase
+ * @n: num
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n == 1 || 0)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (checker(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int square_root(int n, int m);
 int _sqrt_recursion(int n);
+int checker(int n, int i);
+int is_prime_number(int n);
