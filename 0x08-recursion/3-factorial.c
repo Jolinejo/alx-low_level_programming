282f6bf27ef4e82f4356c31ba588f4102292b40f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-12 09:55:09 -0700

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial- Entry point
+ * Description: to check if letter is uppercase
+ * @n: number
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n > 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
