ed0696223eb8822127baab8fa6f79659358f7cec
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-12 19:11:32 -0700

    task

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+ * Description: to concat two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: dest modified
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	int l = 0;
+
+	for (i = 0; dest[i]; i++)
+		l++;
+	for (i = l, j = 0; src[j]; j++, i++)
+		dest[i] = src[j];
+	dest[i] = src[j];
+	return (dest);
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ * Description: to concat two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: dest modified
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+	int l = 0;
+
+	for (i = 0; dest[i]; i++)
+		l++;
+	for (i = l, j = 0; src[j] && j < n; j++, i++)
+		dest[i] = src[j];
+	dest[i] = 0;
+	return (dest);
+}
