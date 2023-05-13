0f461d03462f1931eacd7af923b0cc6292235db6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-12 17:25:51 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: to check if letter is uppercase
+ * @argc: num of inputs
+ * @argv: inputs
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
