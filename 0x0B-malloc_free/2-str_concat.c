d4503476f98a6bbe63ca13d265a9475ac84e7c44
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-16 15:53:21 -0700

    task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _ str_concat - Entry point
+ * Description: to check if letter is uppercase
+ * @s1: string1
+ * @s2: string2
+ * Return: int
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned i, l, l2 = 0;
+	char *str;
+	
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; s++)
+		l++;
+	for (i = 0; s2[i]; s++)
+		l2++;
+	l2++;
+	str = malloc(sizeof(char) * (l + l2));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+		str[i] = s1[i];
+	for (i = 0; s2[i]; i++)
+		str[i] = s2[i];
+	str[i] = s2[i];
+	return (str);
+	
+}
