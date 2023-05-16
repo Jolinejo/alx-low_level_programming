cd12e61986c3f5c09ffc60336e796199cc990c62
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-16 15:12:04 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	int i;
 
-	if (size == NULL)
-		return (0);
+	if (size == 0)
+		return (NULL);
 	ar = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		ar[i] = c;
