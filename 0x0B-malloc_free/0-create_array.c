027285435d5ada52f588043d7e4a4eea6b8f4f45
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-16 15:10:43 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_array - Entry point
+ * Description: to check if letter is uppercase
+ * @size: size
+ * @c: intialize
+ * Return: int
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	int i;
+
+	if (size == NULL)
+		return (0);
+	ar = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+	return (ar);
+}
