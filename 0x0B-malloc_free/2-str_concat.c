bd18163a39d35a6e9e4fb5cfef90becef58e6f9c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-16 16:07:03 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,18 +18,18 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (str);
 	}
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1 != NULL && s1[i]; i++)
 		l++;
-	for (i = 0; s2[i]; i++)
+	for (i = 0; s2 != NULL && s2[i]; i++)
 		l2++;
 	l2++;
 	str = malloc(sizeof(char) * (l + l2));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1 != NULL && s1[i]; i++)
 		str[i] = s1[i];
-	for (j = 0; s2[j]; i++, j++)
+	for (j = 0; s2 != NULL && s2[j]; i++, j++)
 		str[i] = s2[j];
-	str[i] = s2[j];
+	str[i] = '\0';
 	return (str);
 }
