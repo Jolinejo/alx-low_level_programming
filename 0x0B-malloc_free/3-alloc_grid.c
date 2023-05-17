7e993d82f358124760fa2c44d12ba136f3051556
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-16 18:14:47 -0700

    task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid- Entry point
+ * Description: to check if letter is uppercase
+ * @width: w
+ * @height: h
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
