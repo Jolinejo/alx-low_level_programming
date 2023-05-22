c1231acc5a8e1b6cd304e695f11a1cfd370ea173
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-22 15:25:11 -0700

    task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - Entry point
+ * Description: to check if letter is uppercase
+ * @s: string to count its length
+ * Return: no return
+ */
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i]; i++)
+		len++;
+	return (len);
+}
+/**
+ * _strcpy - Entry point
+ * @src: source striing
+ * @dest: dest string
+ * Description: to check if letter is uppercase
+ * Return: no return
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = src[i];
+	return (dest);
+}
+/**
  * new_dog- Entry point
  * Description: to check if letter is uppercase
  * @name: na
@@ -13,11 +44,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new = malloc(sizeof(dog_t));
 
-	if (new == NULL)
+	if (new == NULL || name == NULL || owner == NULL)
 		return (NULL);
-	if (name)
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new->name)
 	{
-		new->name = name;
+		new->name = _strcpy(new->name, name);
 	}
 	else
 	{
@@ -25,8 +57,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	new->age = age;
+	new->owner = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (owner)
-		new->owner = owner;
+		new->owner = _strcpy(new->owner, owner);
 	else
 	{
 		free(new->name);
