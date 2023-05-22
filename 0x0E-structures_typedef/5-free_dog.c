1ad18101230cc8b97f42d81e104fc8855d8e3341
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-22 15:37:09 -0700

    task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog- Entry point
+ * Description: to check if letter is uppercase
+ * @d: do
+ * Return: int
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
