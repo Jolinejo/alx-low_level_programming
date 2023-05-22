ed72e697d11d1fc61d6ae24ea4648b7a83f89c62
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-22 14:38:33 -0700

    task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog- Entry point
+ * Description: to check if letter is uppercase
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: int
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
