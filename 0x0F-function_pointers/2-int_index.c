1d310510b27889c7a50cade57e91a9f286c3480f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-05-24 13:47:19 -0700

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array && cmp)
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			if (cmp(arr[i])
+			if (cmp(array[i]))
 					return (i);
 		}
 	return (-1);
