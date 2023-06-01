36df6f0c6a18eeaebc486c7d139aea76fff34d1a
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-01 13:15:42 -0700

    task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_list - Entry point
+ * Description: to check if letter is uppercase
+ * @h: head
+ * Return: int
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
