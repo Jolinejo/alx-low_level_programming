92b3c4653fa70b6fb19be8d87fb960b13111c1a9
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-01 14:28:41 -0700

    task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * free- Entry point
+ * freem- Entry point
  * Description: to check if letter is uppercase
  * Return: int
  */
-void free(void)
+void freem(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
