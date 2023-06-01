0db4833040d10938d377349a5a16c92d29362260
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-01 13:57:24 -0700

    task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * _strlen - Entry point
+ * Description: to check if letter is uppercase
+ * @s: string to count its length
+ * Return: no return
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+	unsigned int len = 0;
+
+	for (i = 0; s[i]; i++)
+		len++;
+	return (len);
+}
+/**
+ * add_node_end - Entry point
+ * Description: to check if letter is uppercase
+ * @head: head
+ * @str: string
+ * Return: int
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	char *s;
+	unsigned int l;
+
+	if (new == NULL || head == NULL)
+		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = s;
+	l = _strlen(s);
+	new->len = l;
+	while (*head->next)
+		*head = *head->next;
+	new->next = NULL;
+	*head->next == new;
+	return (new);
+
+}
