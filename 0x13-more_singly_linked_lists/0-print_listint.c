e9a3a2d0a00552f157a84495a37a586dcc1a7107
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-05 22:20:32 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	if (h == NULL)
 		return (0);
-	while (h)
+	while (temp)
 	{
-		printf("[%d]\n", h->n);
+		printf("[%d]\n", temp->n);
 		temp = temp->next;
 		i++;
 	}
