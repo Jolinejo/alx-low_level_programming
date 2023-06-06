07619942aa4f5df0da8525cbb53977ffd1b60206
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-06 14:16:02 -0700

    task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listnint- Entry point
+ * Description: to check if letter is uppercase
+ * @head: head
+ * Return: int
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp1 = head;
+	listint_t *temp2;
+
+	if (head == NULL)
+		return;
+	while (temp1)
+	{
+		temp2 = temp1;
+		temp1 = temp1->next;
+		free(temp2);
+	}
+
+}
