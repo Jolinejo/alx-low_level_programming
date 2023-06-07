66dce6533030eb5b42a7694140a6a85827160328
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-06 17:46:20 -0700

    task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if (index < 0 || (index == 0 && head == NULL))
+	if (index == 0 && head == NULL)
 		return (NULL);
 	while (index && head)
 	{
