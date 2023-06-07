b27ecffd68b122fa430dc4d369919762121b6c38
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-06 17:26:13 -0700

    task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,13 +8,13 @@
 int pop_listint(listint_t **head)
 {
 	int i;
-	listint_t temp;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 	i = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
-	head = temp;
+	*head = temp;
 	return (i);
 }
