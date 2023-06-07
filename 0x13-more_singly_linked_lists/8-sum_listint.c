ac180690508145a21b09d84dab88398cd4a15395
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-06 17:49:49 -0700

    task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - Entry point
+ * Description: to check if letter is uppercase
+ * @head: head
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	int x = 0;
+	listint_t *temp = h;
+
+	if (h == NULL)
+		return (0);
+	while (temp)
+	{
+		x += temp->n;
+		temp = temp->next;
+	}
+	return (x);
+}
