a67072863a869500fb0a5ce07089ba9eec9318b2
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-06 19:55:54 -0700

    Taskk

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,8 +38,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(new);
 		return (NULL);
 	}
-	temp->next = new;
 	new->next = temp->next;
+	temp->next = new;
 	new->n = n;
 	return (new);
 }
