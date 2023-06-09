8d8603c7d4e1963e939636672b2da447b0d20af1
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-08 17:24:09 -0700

    task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,15 +9,13 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	if (index > 53)
 		return (-1);
-	while (i != index && n != 0)
+	while (i != index)
 	{
 		i++;
 		n = n >> 1;
 	}
-	if (i == index)
-		return (n & 1);
-	return (-1);
+	return (n & 1);
 }
