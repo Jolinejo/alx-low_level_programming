ab947541250f3fb868238b9ec3562c7a30c589b6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-13 13:52:35 -0700

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	open_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (open_file == -1)
 		return (-1);
-	if(text_content != NULL)
+	if (text_content != NULL)
 		write_status = write(open_file, text_content, strlen(text_content));
 	if (write_status == -1)
 	{
