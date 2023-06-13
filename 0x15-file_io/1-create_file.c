6e8187e8489dae4db161d76fae3cd8123dad9fcb
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-13 13:47:36 -0700

    task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - Entry point
+ * Description: to check if letter is uppercase
+ * @filename: name
+ * @text_content: let
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t open_file, write_status;
+
+	if (filename == NULL)
+		return (-1);
+	open_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (open_file == -1)
+		return (-1);
+	write_status = write(open_file, text_content, strlen(text_content));
+	if (write_status == -1)
+	{
+		close(open_file);
+		return (-1);
+	}
+	close(open_file);
+	return (1);
+}
