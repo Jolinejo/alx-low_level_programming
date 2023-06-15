fca99b875c393f462e569b5db09f9917e2e35a5d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-06-15 11:52:23 -0700

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,46 +8,44 @@
  */
 int main(int argc, char *argv[])
 {
-	int read_status, open_file, open_file2;
-	int close_status, close_status2, write_status, buff;
+	int read_s, open_f, open_f2, close_s, close_s2, write_status, buff;
 	char *file_content;
 
 	buff = 1024;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_content = malloc(buff);
-	open_file = open(argv[1], O_RDONLY);
-	read_status = read(open_file, file_content, buff);
-	open_file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	open_f = open(argv[1], O_RDONLY);
+	read_s = read(open_f, file_content, buff);
+	open_f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	do {
-		if (read_status == -1 || open_file == -1)
+		if (read_s == -1 || open_f == -1)
 		{
 			free(file_content);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		write_status = write(open_file2, file_content, read_status);
-		if (write_status == -1 || open_file2 == -1)
+		write_status = write(open_f2, file_content, read_s);
+		if (write_status == -1 || open_f2 == -1)
 		{
 			free(file_content);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-		read_status = read(open_file, file_content, buff);
-		open_file2 = open(argv[2], O_WRONLY | O_APPEND);
-	} while (read_status > 0);
-	close_status = close(open_file);
-	close_status2 = close(open_file2);
+		read_s = read(open_f, file_content, buff);
+		open_f2 = open(argv[2], O_WRONLY | O_APPEND);
+	} while (read_s > 0);
+	close_s = close(open_f);
+	close_s2 = close(open_f2);
 	free(file_content);
-	if (close_status2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_file2);
-	else if (close_status == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_file);
-	if (close_status2 == -1 || close_status == -1)
+	if (close_s2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_f2);
+	else if (close_s == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_f);
+	if (close_s2 == -1 || close_s == -1)
 		exit(100);
 	return (0);
 }
