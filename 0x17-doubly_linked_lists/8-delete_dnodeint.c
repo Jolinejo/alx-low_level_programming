1f797b37b4aedf8ee3fa5cf3a0ccd41d61735be7
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-20 10:42:33 -0700

    task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index- Entry point
+ * delete_dnodeint_at_index- Entry point
  * Description: to check if letter is uppercase
  * @head: head
  * @index: index
@@ -8,12 +8,14 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	listint_t *temp;
+	dlistint_t *temp;
+	dlistint_t *del;
 
 	if (index == 0)
 	{
 		temp = (*head)->next;
-		temp->prev = NULL;
+		if (temp != NULL)
+			temp->prev = NULL;
 		free(*head);
 		*head = temp;
 		return (1);
@@ -33,9 +35,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	temp->next = temp->next->next;
-	if (temp->next != NULL)
-		temp->next->prev = temp;
-	free(temp->next);
+	del = temp->next;
+	if (del->prev != NULL)
+		del->prev->next = del->next;
+	if (del->next != NULL)
+		del->next->prev = del->prev;
+	free(del);
 	return (1);
 }
