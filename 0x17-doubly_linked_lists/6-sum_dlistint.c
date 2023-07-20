7bd289d9a0adc9154fbe637efe8c0491bf81156e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-20 10:07:01 -0700

    task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Entry point
+ * Description: to check if letter is uppercase
+ * @head: head
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int x = 0;
+	listint_t *temp = head;
+
+	if (head == NULL)
+		return (0);
+	while (temp)
+	{
+		x += temp->n;
+		temp = temp->next;
+	}
+	return (x);
+}
