9e2c9427308295d15a9eb789bd4032bcd1358543
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-20 09:48:31 -0700

    task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - Entry point
+ * Description: print
+ * @head: head
+ * Return: int
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = *head;
+
+	while (head != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
