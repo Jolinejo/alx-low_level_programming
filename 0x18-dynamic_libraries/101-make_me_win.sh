3bd49929413985759317a9e2af567d3fda9478f0
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-24 12:30:44 -0700

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fpic -shared printf.c -o printf.so
-LD_PRELOAD=./printf.so
+wget -P /tmp https://raw.githubusercontent.com/Jolinejo/alx-low_level_programming/master/0x18-dynamic_libraries/printf.so
+LD_PRELOAD=/tmp/printf.so
