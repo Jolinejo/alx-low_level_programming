88c99c14cb34037d3daaa547c77471fca975be8d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-10 08:19:24 -0700

    Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
+	if (ht->array == NULL)
+		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 
 	node = add_node(&(ht->array[idx]), value, key);
