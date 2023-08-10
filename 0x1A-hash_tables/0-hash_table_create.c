ea2473b9dc3ef93bbfdf5107ed436fd253640e30
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-10 05:27:16 -0700

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
-	hash_node_t **arr;
 	unsigned long int i;
 
 	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
 		return (NULL);
-	*arr = malooc(size * sizeof(hash_node_t *));
-	if (*arr == NULL)
+	hash_t->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_t->array == NULL)
 	{
 		free(hash_t);
 		return (NULL);
 	}
 	hash_t->size = size;
-	hash_t->array = arr;
 	for (i = 0; i < size; i++)
 		hash_t->array[i] = NULL;
 	return (hash_t);
