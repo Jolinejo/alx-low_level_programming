eae22d139b75b6d7de9059872e019481d6fb7c04
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-28 07:06:04 -0700

    task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+per
+"""
+
+
+def island_perimeter():
+    """calcs the per"""
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                per += 4
+                if i != 0 and grid[i-1][j] == 1:
+                    per -= 2
+                if j != 0 and grid[i][j-1] == 1:
+                    per -= 2
+    return per
